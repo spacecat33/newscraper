86c184f44d0a2ba016a6de743e9cdb7569e305d2
Janan Akkad <jananakkad@hotmail.co.uk>
2022-08-09 12:01:33 +0100

    added cigar_brands variable

--- scraper.rb
+++ scraper.rb
@@ -7,14 +7,9 @@ class Scraper
     def initialize
         html = URI.open("http://www.cigargeeks.com/cigardb/default.asp?") #search 'brands' of cigars instead of reviews
         parsed_content = Nokogiri::HTML(content)
-        parsed_content.css('.bbstable .messagecellbody') 
+        cigar_brands = parsed_content.css('.bbstable .messagecellbody') 
 
-        # parsed_content.css('td .messagecellbody').css('img')[6].values    #the array number and the format (values) can change
-        # parsed_content.css('.bbstable td').css('img')[6]             # same as above
-        # parsed_content.css('td .messagecellbody').css('img')         # this gives the star ratings - but in 'values' not 'text'
-        # parsed_content.css('.bbstable td')[6]                      # working from 1 to 6 shows the Cigar rating given by individual cigar geek members against a specific cigar.
-        # parsed_content.css('.messagecellbody').css('td')[2].text   # gives same results as above
-        # parsed_content.css('td .messagecellbody')[2].text          # and this
+        
         binding.pry
         
 
@@ -25,3 +20,11 @@ class Scraper
 end
 
 
+# For Review pages:
+# ("http://www.cigargeeks.com/reviews/default.asp?action=showsearch") #or ("http://www.cigargeeks.com/reviews/default.asp")#list of all reviews of different cigar brands
+        # parsed_content.css('td .messagecellbody').css('img')[6].values    #the array number and the format (values) can change
+        # parsed_content.css('.bbstable td').css('img')[6]             # same as above
+        # parsed_content.css('td .messagecellbody').css('img')         # this gives the star ratings - but in 'values' not 'text'
+        # parsed_content.css('.bbstable td')[6]                      # working from 1 to 6 shows the Cigar rating given by individual cigar geek members against a specific cigar.
+        # parsed_content.css('.messagecellbody').css('td')[2].text   # gives same results as above
+        # parsed_content.css('td .messagecellbody')[2].text          # and this
