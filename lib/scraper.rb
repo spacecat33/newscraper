7e74d9bf1b8d89e3171442fbbdbc02cab1647bc4
Janan Akkad <jananakkad@hotmail.co.uk>
2022-09-06 18:17:51 +0100

    updated scraper, brand_name, user_interface and the environment file. Decided not to use the cigar file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,38 +1,28 @@
-# require "nokogiri"
-# require "open-uri"
-# require "pry"
-
-#no longer needed as put it in the environment file.
-
 class Scraper # the scraper class will scrape the data from the website and then parse all the brand names (and their details?) into another class: brand_names folder (and maybe send the names to a separate cigar_info_class?
 
-    def initialize  #or change from initialize to: def brand_from_url as this class will only focus on scraping and sending the data/not using the data? [see phase 1 lesson "collaborating objects lab"] - however may need an argument of page number
+    def get_page  # changed from initialize as this class will only focus on scraping and sending the data/not using the data? [see phase 1 lesson "collaborating objects lab"] - however may need an argument of page number
         html = URI.open("http://www.cigargeeks.com/cigardb/default.asp?") #search 'brands' of cigars instead of reviews
-        parsed_content = Nokogiri::HTML(html)
+        parsed_content = Nokogiri::HTML(html) # this is the HTML (obtained by URI.open) converted into a structure of nested nodes by Nokogiri.
 
-        brands_info = parsed_content.css('.bbstable .messagecellbody') 
-        brands_info.css("a").count #should return '51' as there are 51 brands - not sure if it will be able to count this before the iterating method below or after? Probably doesn't matter as has a css selector 'a'.
-
-        brands_info.each do |course|
-            puts brands_info.text.strip
-        end
-
-
-        cigars_info = parsed_content.css('.bbstable tr')
-        name_of_cigar = cigars_info.css('td')[10].text  #add in a cigar counter?
-        brand_of_cigar = brands_info.css('a')[1].text
-        #link_to_cigar = html + brands_info.css('a')[1]['href']
-        binding.pry
+        brands_info = parsed_content.css('.bbstable .messagecellbody') #The .css method takes in an argument of the CSS selector I want to retrieve. 
         
+        brands = brands_info.css("a")
+        puts brands
+       
+        brands.count #should return '51' as there are 51 items in the array (the first page of the website)- not sure if it will be able to count this before the iterating method below or after? Probably doesn't matter as has a css selector 'a'.
 
-        brands_info.each do |course|
-            puts brands_info.text.strip
+        brands.each do |brand|
+            puts brands.text.strip
         end
 
+        # cigars_info = parsed_content.css('.bbstable tr') #<tr> is CSS for rows (<td> for columns)
+        # name_of_cigar = cigars_info.css('td')[10].text  #add in a cigar counter?
+        # brand_of_cigar = brands_info.css('a')[1].text
+        #link_to_cigar = html + brands_info.css('a')[1]['href']
 
     end
 end
-
+Scraper.new.get_page
 # For Brands pages:
 # brand_name = parsed_content.css('a').text
 # brand_details = parsed_content.css('.bbstable tr') - with each specific heading e.g length[1], color[6] etc, bring .css('td')[#] e.g. [7] = strength
